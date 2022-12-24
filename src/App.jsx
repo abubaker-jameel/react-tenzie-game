eb45c024c59631c659da5fc9dfd8c360cdc79421
abubaker <codecookies958@gmail.com>
2022-12-24 19:30:37 +0500

    style: design the tenzie-game

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,27 @@
 import React from "react";
+import Die from "./Die";
 
 export default function App() {
-  return <div>App</div>;
+  return (
+    <main>
+      <h1>Tenzies</h1>
+      <p>
+        Roll until all dice are the same. Click each die to freeze it at its
+        current value between rolls.
+      </p>
+      <div className="dice--container">
+        <Die value="1" />
+        <Die value="1" />
+        <Die value="1" />
+        <Die value="1" />
+        <Die value="1" />
+        <Die value="1" />
+        <Die value="1" />
+        <Die value="1" />
+        <Die value="1" />
+        <Die value="1" />
+      </div>
+      <button className="roll-dice">Roll</button>
+    </main>
+  );
 }
