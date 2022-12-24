9b1747e9669fbaa46e6142f57be55fcfa54cd31f
abubaker <codecookies958@gmail.com>
2022-12-25 00:23:10 +0500

    feat: added the functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,76 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { nanoid } from "nanoid";
+import Confetti from "react-confetti";
 import Die from "./Die";
 
 export default function App() {
+  const [dice, setDice] = useState(allNewFaces());
+  const [tenzies, setTenzies] = useState(false);
+
+  useEffect(() => {
+    const firstValue = dice[0].value;
+    const allSame = dice.every((die) => die.value === firstValue);
+    const allHeld = dice.every((die) => die.isHeld === true);
+    if (allSame && allHeld) {
+      setTenzies(true);
+      console.log("You won");
+    }
+  }, [dice]);
+
+  function generateRandomFaces() {
+    return {
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false,
+      id: nanoid(),
+    };
+  }
+  function allNewFaces() {
+    const diceFaces = [];
+    for (let i = 0; i < 10; i++) {
+      diceFaces.push(generateRandomFaces());
+    }
+    return diceFaces;
+  }
+
+  const allDices = dice.map((dice) => (
+    <Die
+      key={dice.id}
+      value={dice.value}
+      holdDice={() => isHeld(dice.id)}
+      isHeld={dice.isHeld}
+    />
+  ));
+
+  function rollDice() {
+    if (!tenzies) {
+      setDice((prevDice) =>
+        prevDice.map((die) => (die.isHeld ? die : generateRandomFaces()))
+      );
+    } else {
+      setTenzies(false);
+      setDice(allNewFaces());
+    }
+  }
+
+  function isHeld(id) {
+    setDice((prevDice) =>
+      prevDice.map((die) =>
+        die.id === id ? { ...die, isHeld: !die.isHeld } : die
+      )
+    );
+  }
   return (
     <main>
+      {tenzies && <Confetti />}
       <h1>Tenzies</h1>
       <p>
         Roll until all dice are the same. Click each die to freeze it at its
         current value between rolls.
       </p>
-      <div className="dice--container">
-        <Die value="1" />
-        <Die value="1" />
-        <Die value="1" />
-        <Die value="1" />
-        <Die value="1" />
-        <Die value="1" />
-        <Die value="1" />
-        <Die value="1" />
-        <Die value="1" />
-        <Die value="1" />
-      </div>
-      <button className="roll-dice">Roll</button>
+      <div className="dice--container">{allDices}</div>
+      <button className="roll-dice" onClick={rollDice}>
+        {tenzies ? "New Game" : "Roll"}
+      </button>
     </main>
   );
 }
--- src/Die.jsx
+++ src/Die.jsx
@@ -1,9 +1,75 @@
 import React from "react";
 
 export default function Die(props) {
+  const styles = {
+    backgroundColor: props.isHeld === true ? "#59E391" : "white",
+  };
   return (
-    <div className="dice--face">
-      <h1>{props.value}</h1>
+    <div className="dice--section">
+      {props.value === 1 && (
+        <div className="first-face" style={styles} onClick={props.holdDice}>
+          <span className="pip"></span>
+        </div>
+      )}
+
+      {props.value === 2 && (
+        <div className="second-face" style={styles} onClick={props.holdDice}>
+          <span className="pip"></span>
+          <span className="pip"></span>
+        </div>
+      )}
+
+      {props.value === 3 && (
+        <div className="third-face" style={styles} onClick={props.holdDice}>
+          <span className="pip"></span>
+          <span className="pip"></span>
+          <span className="pip"></span>
+        </div>
+      )}
+
+      {props.value === 4 && (
+        <div className="fourth-face" style={styles} onClick={props.holdDice}>
+          <div className="column">
+            <span className="pip"></span>
+            <span className="pip"></span>
+          </div>
+          <div className="column">
+            <span className="pip"></span>
+            <span className="pip"></span>
+          </div>
+        </div>
+      )}
+
+      {props.value === 5 && (
+        <div className="fifth-face" style={styles} onClick={props.holdDice}>
+          <div className="column">
+            <span className="pip"></span>
+            <span className="pip"></span>
+          </div>
+          <div className="column">
+            <span className="pip"></span>
+          </div>
+          <div className="column">
+            <span className="pip"></span>
+            <span className="pip"></span>
+          </div>
+        </div>
+      )}
+
+      {props.value === 6 && (
+        <div className="sixth-face" style={styles} onClick={props.holdDice}>
+          <div className="column">
+            <span className="pip"></span>
+            <span className="pip"></span>
+            <span className="pip"></span>
+          </div>
+          <div className="column">
+            <span className="pip"></span>
+            <span className="pip"></span>
+            <span className="pip"></span>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
